64e286767f046ab50cf848faecd17f3641b5d549
pandeydivyanshi <divyanshipandey0111@gmail.com>
2025-06-04 16:46:32 +0530

    trialinput

---
+++ trialinput.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number:"))
+b=float(input("enter second number"))
+print("sum is",a+b)
+print("difference is",a-b)
+print("product is",a*b)
+print("division is ",a/b)
+
+#area of square
+side=float(input("enter side of square:"))
+print("area is ",side*side)
+
