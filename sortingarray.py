ddcc14a4aa1b5a22e3627bd1d9b365320a48ca45
pandeydivyanshi <divyanshipandey0111@gmail.com>
2025-06-18 13:49:40 +0530

    sorting of array

---
+++ sortingarray.py
@@ -0,0 +1,19 @@
+##in numpy fnc sort() use for sorting array
+import numpy as np
+x=np.array([1111,222,33,34,79,7875,9867])
+print(np.sort(x))
+
+##sorting of alphabets
+import numpy as np
+x=np.array(['sun','moon','earth','divyanshi','asman','jamin'])
+print(np.sort(x))
+
+##sorting boolean value
+import numpy as np
+x=np.array([True,False,True,False])
+print(np.sort(x))
+
+##sorting of 2D array
+import numpy as np
+x=np.array([[1,2,3,6756],[212,434,25,235]])
+print(np.sort(x))
