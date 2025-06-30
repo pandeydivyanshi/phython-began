5c10070027f60bff62e5e4c552811e7fb8c18aab
divyanshi <divyanshipandey0111@gmail.com>
2025-06-30 16:55:30 +0530

    Delete multinomial_dist.py

--- multinomial_dist.py
+++
@@ -1,7 +0,0 @@
-##multinomial distribution
-##parameter:n(no.of trials),pvals(list of outcomes or possibility),size
-from numpy import random
-x=random.multinomial(n=50,pvals=[1/9,2/9,3/9,3/9])
-print(x)
-
-##multinomial will not provide single value
